0e90a02d232dec61b1e04adf633fb6c2dad0413d
Adewale Azeez <azeezadewale98@gmail.com>
2020-01-04 19:26:03 +0100

    test for listing running windows process and add microsoft api url

--- test/windows/running_process.cpp
+++ test/windows/running_process.cpp
@@ -17,12 +17,27 @@ void ListRunningProcess() {
     pe32.dwSize = sizeof(PROCESSENTRY32);
 
     while (Process32Next(hProcessSnap, &pe32)) { 
-        printf("%s\n", pe32.szExeFile);
+        printf("%s, %i, %i\n", pe32.szExeFile, pe32.th32ProcessID, pe32.cntThreads);
     }
     CloseHandle(hProcessSnap);
 }
 
 /**
+https://docs.microsoft.com/en-us/windows/win32/api/tlhelp32/
+https://docs.microsoft.com/en-us/windows/win32/api/tlhelp32/nf-tlhelp32-process32next
+https://docs.microsoft.com/en-us/windows/win32/api/tlhelp32/ns-tlhelp32-processentry32w
 
+typedef struct tagPROCESSENTRY32 {
+  DWORD     dwSize;
+  DWORD     cntUsage;
+  DWORD     th32ProcessID;
+  ULONG_PTR th32DefaultHeapID;
+  DWORD     th32ModuleID;
+  DWORD     cntThreads;
+  DWORD     th32ParentProcessID;
+  LONG      pcPriClassBase;
+  DWORD     dwFlags;
+  CHAR      szExeFile[MAX_PATH];
+} PROCESSENTRY32;
 
-**/
+*/
