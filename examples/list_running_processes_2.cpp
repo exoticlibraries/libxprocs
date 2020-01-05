09e8a8ecfe1792c8d4b473c0dab93ee79e9f4348
Adewale Azeez <azeezadewale98@gmail.com>
2020-01-05 16:45:24 +0100

    example with a working callback functiopn and extra param

---
+++ examples/list_running_processes_2.cpp
@@ -0,0 +1,27 @@
+/**
+    \copyright GNU General Public License v3.0 Copyright (c) 2019, Adewale Azeez 
+    \author Adewale Azeez <azeezadewale98@gmail.com>
+    \date 06 January 2019
+    \file list_running_processes_2.cpp
+*/
+
+#include "../src/libo-processes.cpp"
+#include <iostream>
+
+bool SpecificAppnameCondition(PROCESS process, void* extraParam )
+{
+    if (process.exeName == ((char*) extraParam))
+    {
+        return true;
+    }
+    return false;
+}
+
+int main() 
+{
+    std::list<PROCESS> processes = RunningProcesses(&SpecificAppnameCondition, (char*)"brave.exe");
+    std::list<PROCESS>::iterator it; 
+    for(it = processes.begin(); it != processes.end(); ++it) 
+        std::cout << "Id=" << it->Id << "," << it->exeName << "," << it->exePath << std::endl;
+    return 0;
+}
